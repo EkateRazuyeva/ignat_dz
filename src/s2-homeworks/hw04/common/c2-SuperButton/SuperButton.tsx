202d8b3b1a1ffd9f20dbead3e72f23c269e0a45d
Ekate Razuyeva <25ky-ky08@mail.ru>
2023-08-31 21:17:41 +0300

    hm_04_2

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -24,8 +24,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
                 ? ' ' + s.red
                 : xType === 'secondary'
                     ? ' ' + s.secondary
-                    : xType === 'default'
-                        ? ' ' + s.default : '')
+                    :  ' ' + s.default )
         + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
