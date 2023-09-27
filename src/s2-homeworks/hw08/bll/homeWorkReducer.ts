4b472ff169d3f32264c64b2e89dd8069f7f5bf3a
Ekate Razuyeva <25ky-ky08@mail.ru>
2023-09-27 21:01:41 +0300

    hm_08_4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -9,14 +9,14 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
         case 'sort': { // by name
 
             if (action.payload === 'up') {
-                return state.sort((x, y) => x.name.localeCompare(y.name))
+                return [...state].sort((x, y) => x.name.localeCompare(y.name))
             } else {
-                return state.sort((x, y) => y.name.localeCompare(x.name))
+                return [...state].sort((x, y) => y.name.localeCompare(x.name))
             }// need to fix
         }
         case 'check': {
-            const newState = state.filter(s => s.age > 18)
-            return newState.sort((a, b) => a.age - b.age)// need to fix
+
+            return [...state].sort((a, b) => a.age - b.age).filter(s => s.age > 18)// need to fix
         }
         default:
             return state
