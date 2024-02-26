387133fde13779fb5f803bf1e0f9d44c77a5d77c
Ekate Razuyeva <25ky-ky08@mail.ru>
2024-02-26 22:44:01 +0300

    hw_14

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -33,10 +33,13 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
 
         if (onDebouncedChange) {
             // делает студент
-
+            clearTimeout(timerId)
+            const id = setTimeout(() => {
+                onDebouncedChange(value)
+            }, 1500)
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
-
+            setTimerId(+id)
             //
         }
     }
